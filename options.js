d3b9904eb93a81572d2d4accbefbd1231d5266e6
imperatrona <xb2w1z@gmail.com>
2022-10-20 08:16:50 +0300

    accessability title fixes

--- options.js
+++ options.js
@@ -154,7 +154,7 @@ function rebuildSelects() {
               `<select id="select_group-${i}" class="select_group">${groupList.join('')}</select>` +
               `<select id="select_album-${i}" class="select_album">${albumList.join('')}</select>` +
               `</div>` +
-              `<button class=delete id=delete-${i}><svg class=icon fill=none height=24 width=24 xmlns=http://www.w3.org/2000/svg><g stroke-linecap=round stroke-linejoin=round stroke-width=1.5><path d="M20.25 5.25H3.75M9.75 9.75v6M14.25 9.75v6M18.75 5.25V19.5a.75.75 0 0 1-.75.75H6a.75.75 0 0 1-.75-.75V5.25M15.75 5.25v-1.5a1.5 1.5 0 0 0-1.5-1.5h-4.5a1.5 1.5 0 0 0-1.5 1.5v1.5"/></g></svg></button>` +
+              `<button class="delete" id="delete-${i}"><svg class="icon" fill="none" height="24" width="24" xmlns="http://www.w3.org/2000/svg"><title>Удалить</title><g stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M20.25 5.25H3.75M9.75 9.75v6M14.25 9.75v6M18.75 5.25V19.5a.75.75 0 0 1-.75.75H6a.75.75 0 0 1-.75-.75V5.25M15.75 5.25v-1.5a1.5 1.5 0 0 0-1.5-1.5h-4.5a1.5 1.5 0 0 0-1.5 1.5v1.5"/></g></svg></button>` +
               `</div>`)
   }
   ge('albums-block').innerHTML = list.join('');
